dcd6dd4b78645b0ee2ebb75b80eb5fe54665080f
Sander <mail@sanderboer.nl>
2025-05-30 14:56:58 +0200

    use cloudinary for media

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,16 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "local",
+      provider: "cloudinary",
       providerOptions: {
-        sizeLimit: 100000,
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
