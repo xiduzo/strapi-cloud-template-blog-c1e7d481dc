f32bd8f6f8d35cbe537244887eb195fb78b07be5
Sander <mail@sanderboer.nl>
2025-06-16 21:47:25 +0200

    added media-prefix plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'local',
+      provider: "local",
       sizeLimit: 4_000_000,
     },
+  },
+  "media-prefix": {
+    enabled: true,
   },
 });
